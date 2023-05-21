cb9f06b886dea7bae6a1b0d1e5c46f798754c882
Lee <Lee@DESKTOP-JLIF73D>
2023-05-21 18:32:28 +0900

    Perifocal frame to ECI frame

---
+++ Perifocal2ECI.m
@@ -0,0 +1,20 @@
+function Rot_Mat = PQW2ECI(arg_prg,inc_angle,RAAN)
+
+arg_prg = input("argument of perigee = ");
+inc_angle = input("inclination angle = ");
+RAAN = input("Right Ascension of Ascending Node = ");
+
+R1 = [cosd(RAAN) sind(RAAN) 0;
+      -sind(RAAN) cosd(RAAN) 0;
+      0 0 1];
+R2 = [1 0 0;
+      0 cosd(inc_angle) sind(inc_angle);
+      0 -sind(inc_angle) cosd(inc_angle)];
+R3 = [cosd(arg_prg) sind(arg_prg) 0;
+      -sind(arg_prg) cosd(arg_prg) 0;
+      0 0 1];
+
+Rot_Mat = R1*R2*R3;
+
+end
+
