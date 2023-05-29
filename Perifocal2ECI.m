5fe145a815357a662626b428809da7fe30e9197c
Lee <Lee@DESKTOP-JLIF73D>
2023-05-29 18:48:37 +0900

    실행 시 입력값 입력 후 바로 출력되게 변경

--- Perifocal2ECI.m
+++ Perifocal2ECI.m
@@ -1,8 +1,10 @@
-function Rot_Mat = PQW2ECI(arg_prg,inc_angle,RAAN)
-
 arg_prg = input("argument of perigee = ");
 inc_angle = input("inclination angle = ");
 RAAN = input("Right Ascension of Ascending Node = ");
+
+Rot_Mat = PQW2ECI(arg_prg,inc_angle,RAAN)
+
+function Rot_Mat = PQW2ECI(arg_prg,inc_angle,RAAN)
 
 R1 = [cosd(RAAN) sind(RAAN) 0;
       -sind(RAAN) cosd(RAAN) 0;
@@ -17,4 +19,3 @@ R3 = [cosd(arg_prg) sind(arg_prg) 0;
 Rot_Mat = R1*R2*R3;
 
 end
-
